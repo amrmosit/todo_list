071f21596794b18dc1b3008593221093ddd96cdb
Amr Mostafa <amrmos.it@gmail.com>
2025-04-11 17:31:30 +1000

    Implement Add List functionality

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -9,7 +9,15 @@ const TodoList = () => {
   const handleAddTodo = () => {
     if (headingInput.trim() !=='') {
       setTodos([...todos, {heading: headingInput, lists:[]}]);
-      setHeadningInput('');
+      setHeadingInput('');
+    }
+  }
+  const handleAddList = (index)=>{
+    if (listInputs[index] && listInputs[index].trim() !==''){
+      const newTodos = [...todos];
+      newTodos[index].lists.push(listInputs[index]);
+      setTodos(newTodos);
+      setListInputs({...listInputs, [index]:''});
     }
   }
 
@@ -29,16 +37,33 @@ const TodoList = () => {
             onChange={(e) => {setHeadingInput(e.target.value);}} //Add onChange event handler to update headingInput state
             
           />
-          <button className="add-list-button">Add Heading</button>
+          <button className="add-list-button" onClick={handleAddTodo}>Add Heading</button>
         </div>
       </div>
-      
+      {/*display Todo list*/}
       <div className="todo_main">
         {todos.map((todo, indix) =>(
           <div key={indix} className='todo-card'>
             <div className='heading_todo'>
               <h3>{todo.heading}</h3> {/*Display the heading here*/}
               <button className='delet-button-heading' onClick={() => heandleDeleteTodo(indix)}>Delete Heading</button>
+            </div>
+            <div className='add_list'>
+              <input
+                type='text'
+                className='list-input'
+                placeholder='Add List'
+                value={listInputs[indix] || ''}
+                onChange={(e) => handleListInputChange(index, e.target.value)}
+              />
+              <button className='add-list-buttom' onClick={()=> handleAddList(index)}>Add List</button>
+              <ul>
+                [todo.lists.maps((list, listIndex)=> (
+                  <li key={listIndex} className='todo_inside_list'>
+                    <p>{lsit}</p>
+                  </li>
+                ))]
+              </ul>
             </div>
           </div>
         ))}
